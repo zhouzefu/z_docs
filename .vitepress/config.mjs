b20157b65197421df695ac839bf569cfafe94ac2
zhouzefu <zhouzefu@126.com>
2024-12-09 21:33:19 +0800

    初始

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/zhouzefu/z_docs.git' }
     ]
   }
 })
